9b83b1d93101d96e17e97a1b799695003284e9a8
Nestor Recinos <00083120@uca.edu.sv>
2022-11-16 21:46:33 -0600

    :construction: Ask for parameters to user

--- exercise_2.cpp
+++ exercise_2.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-int main() {
+int minimumFuelCalculator(int citiesNumber, int tankMaxCapacity)
+{
+  return 0;
+}
 
+int main()
+{
+  int citiesNumber = 0, tankMaxCapacity =0;
+  cout << "Type the number of cities to visit: ";
+  cin >> citiesNumber;
+  cout << "Type the your tank's maximum capacity of fuel in litters: ";
+  cin >> tankMaxCapacity;
   return 0;
 }
