f84423e8d73db4a32351d3fffe26cf3ee7401fc8
Nestor Recinos <00083120@uca.edu.sv>
2022-11-17 00:07:37 -0600

    :sparkles: Add functionality

--- exercise_2.cpp
+++ exercise_2.cpp
@@ -2,17 +2,33 @@
 
 using namespace std;
 
-int minimumFuelCalculator(int citiesNumber, int tankMaxCapacity)
+int minimum_fuel_calculator(int cities_number, int tank_max_capacity)
 {
-  return 0;
+  int current_litter = 0, required_litters = cities_number - 1, cost = 0;
+  int litters_to_fill = 0;
+
+  for (int i = 1; i < cities_number; i++)
+  {
+    litters_to_fill = min(tank_max_capacity - current_litter, required_litters);
+    current_litter += litters_to_fill;
+    current_litter += -1;
+    cost += (litters_to_fill * i);
+    required_litters -= litters_to_fill;
+  }
+  return cost;
 }
 
 int main()
 {
-  int citiesNumber = 0, tankMaxCapacity =0;
+  int cities_number = 0, tank_max_capacity = 0, minimum_money_needed = 0;
   cout << "Type the number of cities to visit: ";
-  cin >> citiesNumber;
+  cin >> cities_number;
   cout << "Type the your tank's maximum capacity of fuel in litters: ";
-  cin >> tankMaxCapacity;
+  cin >> tank_max_capacity;
+
+  minimum_money_needed = minimum_fuel_calculator(cities_number, tank_max_capacity);
+
+  cout << "You need at least " << minimum_money_needed << " for fuel in order to complete the travel.";
+
   return 0;
 }
